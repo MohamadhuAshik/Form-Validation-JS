6ab7f908a55052722cc43609a06c7ee02d62c99f
MohamadhuAshik <mohamadashik268@gmail.com>
2023-10-10 18:08:50 +0530

    Form-Validate

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function validate() {
+  var fName = document.getElementById("fName");
+  var lName = document.getElementById("lName");
+  var mobile = document.getElementById("number");
+  var email = document.getElementById("email");
+  var nPass = document.getElementById("nPass");
+  var cPass = document.getElementById("cPass");
+
+  if (fName.value == "" || lName.value == "" || email.value == "" || nPass.value == "" || cPass.value == "" || mobile.value == " ") {
+    alert("No Blank Values Allowded");
+    return false;
+  } else {
+    true;
+  }
+}
